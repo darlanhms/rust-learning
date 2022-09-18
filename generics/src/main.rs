962ec8724baa8a25ca2987c708ce74c2c7cce4f0
Darlan H <darlanhermessjb@hotmail.com>
2022-09-18 12:55:12 -0300

    generics

---
+++ generics/src/main.rs
@@ -0,0 +1,23 @@
+// fn largest<T>(list: &[T]) -> T 
+// where 
+//     T: i32,
+// {
+//     let mut largest = &list[0];
+
+//     for item in list {
+//         if item > largest {
+//             largest = item;
+//         }
+//     }
+
+//     largest
+// }
+
+
+// fn main() {
+//     let number_list = vec![34, 50, 25, 100, 65];
+
+//     println!("The largest number is {}", largest(&number_list));
+// }
+fn main() {
+}
